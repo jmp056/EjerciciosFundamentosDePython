5b07d5acd5ddaf3880a2561ab2b34f8f7788acf1
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-08-10 21:19:55 -0400

    Ejercicio 01 terminado

---
+++ Jonathan_Maria/ejercicio_01.py
@@ -0,0 +1,9 @@
+
+def cuenta_palabras(nombre):
+    print(len(nombre.strip().split(' ')))
+
+cuenta_palabras('Probando')
+cuenta_palabras('Esta prueba fue un exito')
+cuenta_palabras('Jonathan Maria Parra')
+cuenta_palabras('Ejercicio numero 1 terminado')
+cuenta_palabras('Juan Perez')
