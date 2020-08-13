4193bbc0f04110dad80e61901eae05edd94306c5
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2020-08-12 21:10:40 -0400

    Ejercicio 08 terminado

---
+++ Jonathan_Maria/ejercicio_08.py
@@ -0,0 +1,17 @@
+articulo = " "
+listado = list()
+unicos = 0
+
+while True:
+    articulo = input()
+    if(articulo == 'parar'):
+        break
+    else:
+        listado.append(articulo)
+
+for art in listado:
+    if(listado.count(art) > 1):
+        unicos += 1
+
+print("La lista contiene un total de", len(listado), "articulos")
+print("La lista contiene un total de", unicos-1, "articulos unicos")
